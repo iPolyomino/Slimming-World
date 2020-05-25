67c795b570d0ee46c7e5a26859a3db6392c22ccc
iPolyomino <macbookpromacbookpromacbookpro@gmail.com>
2020-05-25 12:21:21 +0900

    add bmi status

--- app/routes.php
+++ app/routes.php
@@ -8,6 +8,8 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 use Slim\Interfaces\RouteCollectorProxyInterface as Group;
 
+require 'bmi_status.php';
+
 return function (App $app) {
     $app->options('/{routes:.*}', function (Request $request, Response $response) {
         // CORS Pre-Flight OPTIONS Request Handler
@@ -25,10 +27,10 @@ return function (App $app) {
         $height = $personalData['height'] / 100;
         $weight = $personalData['weight'];
         $bmi = $weight / $height ** 2;
-        $responseData = array('bmi' => $bmi);
+        $responseData = array('bmi' => $bmi, 'status' => bmi_status($bmi));
         $response->getBody()->write(json_encode($responseData));
         return $response->withHeader('Content-Type', 'application/json');
-    }) ;
+    });
 
     $app->group('/users', function (Group $group) {
         $group->get('', ListUsersAction::class);
